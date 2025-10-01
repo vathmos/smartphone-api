f64eafd92ae2b716bc6aaa6b522c440f56cd39f1
vathmos <esqeue@gmail.com>
2025-10-01 21:06:13 +0700

    add validation and error logging

--- src/controllers/phone.controller.ts
+++ src/controllers/phone.controller.ts
@@ -9,48 +9,96 @@ import {
 
 async function getAllSmartphones(req: Request, res: Response) {
   const smartphones = await findSmartphones();
-  res.send(smartphones);
+
+  res.json(smartphones);
 }
 
 async function getSmartphoneById(req: Request, res: Response) {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const smartphone = await findsmartphoneById(Number(id));
+    const smartphone = await findsmartphoneById(Number(id));
 
-  if (!smartphone) {
-    res.status(404).json({ message: "Smartphone not found" });
-    return;
+    if (!smartphone) {
+      res.status(404).json({ message: "Smartphone not found" });
+      return;
+    }
+
+    res.json(smartphone);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send(error.message);
+    } else {
+      res.status(400).send("An unknown error has occured");
+    }
   }
-
-  res.json(smartphone);
 }
 
 async function createSmartphone(req: Request, res: Response) {
-  const smartphoneData = req.body;
-  await insertSmartphone(smartphoneData);
+  try {
+    const smartphoneData = req.body;
 
-  res.status(201).json({
-    data: smartphoneData,
-    message: "Smartphone created successfully",
-  });
+    if (
+      !(
+        smartphoneData.brand &&
+        smartphoneData.model &&
+        smartphoneData.brief &&
+        smartphoneData.released &&
+        smartphoneData.announced &&
+        smartphoneData.hardwareDesign &&
+        smartphoneData.codename
+      )
+    ) {
+      res.status(400).json({ message: "Some fields are missing" });
+    }
+
+    await insertSmartphone(smartphoneData);
+
+    res.status(201).json({
+      data: smartphoneData,
+      message: "Smartphone created successfully",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send(error.message);
+    } else {
+      res.status(400).send("An unknown error has occured");
+    }
+  }
 }
 
 async function editSmartphone(req: Request, res: Response) {
-  const { id } = req.params;
-  const newSmartphoneData = req.body;
-  await updateSmartphone(Number(id), newSmartphoneData);
+  try {
+    const { id } = req.params;
+    const newSmartphoneData = req.body;
+    await updateSmartphone(Number(id), newSmartphoneData);
 
-  res.status(200).json({
-    data: newSmartphoneData,
-    message: "Smartphone updated successfully",
-  });
+    res.status(200).json({
+      data: newSmartphoneData,
+      message: "Smartphone updated successfully",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send(error.message);
+    } else {
+      res.status(400).send("An unknown error has occured");
+    }
+  }
 }
 
 async function deleteSmartphoneById(req: Request, res: Response) {
-  const { id } = req.params;
-  await deleteSmartphone(Number(id));
+  try {
+    const { id } = req.params;
+    await deleteSmartphone(Number(id));
 
-  res.status(200).json({ message: "Smartphone deleted successfully" });
+    res.status(200).json({ message: "Smartphone deleted successfully" });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send(error.message);
+    } else {
+      res.status(400).send("An unknown error has occured");
+    }
+  }
 }
 
 export {
